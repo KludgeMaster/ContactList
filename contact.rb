755d7d9f6ff222bf20e15f7f1e332a48d26c997f
KludgeMaster <yiryans@gmai.com>
2015-08-05 22:59:24 +0000

    contact_list v3 COMPLETEDgit add .git add .

--- contact.rb
+++ contact.rb
@@ -1,121 +1,47 @@
 # require_relative 'contact_database'
-require 'pry'
-require 'pg'
+# require 'pry'
+# require 'pg'
 
 
-class Contact
-  # include DataBaseReadWrite
-  attr_accessor :firstname, :lastname, :email, :id
-  CONN = PG::Connection.new(host: 'localhost', port: 5432, user: 'development',password: 'development', dbname: 'contact_list')
-  
-
-  def initialize(firstname, lastname, email, id =0)
-    # TODO: assign local variables to instance variables
-    @firstname = firstname
-    @lastname = lastname
-    @email = email
-    @id = id if id.to_i > 0
-  end
-
-  #@@contacts = DataBaseReadWrite.read_all_contacts
+class Contact < ActiveRecord::Base
   
   def to_s
-    # TODO: return string representation of Contact
-    # binding.pry
-    "#{id}, #{@firstname.gsub(/^\s+/,"")},#{@lastname.gsub(/^\s+/,"")}, (#{@email.gsub(/\s+/,"")})\n"
+    "#{id}, #{first_name}, #{last_name}, (#{email})\n"
   end
   
-  def save
-    if id
-      # query = "UPDATE contacts SET firstname = '#{firstname}', latename = '#{lastname}', email = '#{email}' WHERE id = #{id};"
-      CONN.exec_params('UPDATE contacts SET firstname = $1, lastname = $2, email = $3 WHERE id = $4',["#{firstname}","#{lastname}","#{email}", "#{id}"])
-    else 
-      # query = "INSERT INTO contacts (firstname, lastname, email) VALUES ('#{firstname}', '#{lastname}', '#{email}');"
-      CONN.exec_params('INSERT INTO contacts (firstname, lastname, email) VALUES ($1, $2, $3)',["#{firstname}","#{lastname}","#{email}"])
-    end
-    # CONN.exec(query)
-  end
-
-  def destroy
-    CONN.exec_params('DELETE FROM contacts WHERE id = $1',["#{id}"])
-    # query = "DELETE FROM contacts WHERE id = #{id};"
-    # CONN.exec(query)
-  end
-
-
   ## Class Methods
   class << self
     
-    # def create(name, email)
-    #   # TODO: Will initialize a contact as well as add it to the list of contacts
-    #   #contacts << Contact.new(name,email) # will this work?
-    #   my_contact = Contact.new(name, email)
-    #   DataBaseReadWrite.add_contact(my_contact.to_s)
-    #   @@contacts << my_contact
-    # end
-    
-    def find(id)
-      # query = "SELECT * FROM contacts WHERE id = #{id};"
-      # temp = CONN.exec(query)
-      result = CONN.exec_params('SELECT * FROM contacts WHERE id = $1;',["#{id}"])
-      # puts temp[0] 
-      # Contact.new(temp[0]["firstname"], temp[0]["lastname"], temp[0]["email"], temp[0]["id"].to_i)
-      convert_to_contacts(result)[0]
+    def search(term)
+      list = []
+      list << find_by_email(term)
+      list << find_all_by_lastname(term)
+      list << find_all_by_firstname(term)
     end
+
+
 
     def find_all_by_lastname(name)
       # query = "SELECT * FROM contacts WHERE lastname LIKE '%#{name}$';" 
       # This will return all rows that include 'name' in their lastname column.
-      result = CONN.exec_params('SELECT * FROM contacts WHERE lastname LIKE $1;',["%#{name}%"])
-      convert_to_contacts(result)
+      # result = CONN.exec_params('SELECT * FROM contacts WHERE lastname LIKE $1;',["%#{name}%"])
+      # convert_to_contacts(result)
+      self.where("last_name LIKE ('%#{name}%')")
     end
 
     def find_all_by_firstname(name)
-      result = CONN.exec_params('SELECT * FROM contacts WHERE firstname LIKE $1;',["%#{name}%"])
-      convert_to_contacts(result)
+      # result = CONN.exec_params('SELECT * FROM contacts WHERE firstname LIKE $1;',["%#{name}%"])
+      # convert_to_contacts(result)
+      self.where("first_name LIKE ('%#{name}%')")
     end
 
     def find_by_email(email)
-      result = CONN.exec_params('SELECT * FROM contacts WHERE email = $1;',["#{email}"])
-      convert_to_contacts(result)
+      # result = CONN.exec_params('SELECT * FROM contacts WHERE email = $1;',["#{email}"])
+      # convert_to_contacts(result)
+      self.where("email LIKE ('%#{email}%')")
     end
 
-    def show_all
-      # Show me everything in the table (it's not much for now)
-      result = CONN.exec_params('SELECT * FROM contacts;')
-      # query = "SELECT * FROM contacts;"
-      # CONN.exec(query)
-      all = convert_to_contacts(result)
-      result_str = []
-      all.each do |contact|
-        result_str << contact.to_s
-      end
-    end
-
-    private
-    def convert_to_contacts (result)
-      contacts=[]
-      result.each do |contact|
-        contacts << Contact.new(contact[:firstname.to_s], contact[:lastname.to_s], contact[:email.to_s], contact[:id.to_s].to_i)
-      end
-      # binding.pry
-      contacts
-    end
-
-    # def all
-    #   # TODO: Return the list of contacts, as is
-    #   DataBaseReadWrite.read_all_contacts
-    #   # @@contacts.each do |contact|
-    # end
-    
-    # def show(id)
-    #   # TODO: Show a contact, based on ID
-    #   DataBaseReadWrite.read_all_contacts.each do |contact|
-    #     contact_split = contact.to_s.split(',')
-    #     puts contact if contact_split[0] == id
-    #   end
-    # end
-    
+   
   end
  
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,43 +1,66 @@
-require_relative 'contact'
-require_relative 'contact_database'
-
 
 require 'pry'
-# TODO: Implement command line interaction
-# This should be the only file where you use puts and gets
+require 'pg'
+require_relative 'setup'
+require_relative 'contact'
+require_relative 'contact_list'
 
-puts "This is Ryan's contact list!"
-puts "What would you like to do?"
-puts "Enter 'Help' for a list of commands."
-print '> '
 
-user_input = gets.chomp
+puts '---------------------------------------------------'
+puts 'Adding some contacts to start with.....'
 
-case user_input
-  # binding.pry
-  when "new"
-    print "Name:"
-    name = gets.chomp
-    print "Email:"
-    email = gets.chomp
-    Contact.create(name,email)
+c1 = Contact.create(first_name: "Ryan", last_name: "Song", email: "yiryans@gmail.com")
+c2 = Contact.create(first_name: "Ashley", last_name: "Chun", email: "ashley@gmail.com")
+c3 = Contact.create(first_name: "Bat", last_name: "Man", email: "imdark@gmail.com")
+c4 = Contact.create(first_name: "Spider", last_name: "Man", email: "ibelieveicanfly@gmail.com")
+c5 = Contact.create(first_name: "Super", last_name: "Man", email: "wasabi_killer@gmail.com")
 
-  when "list"
-    puts Contact.all
-  
-  when "show"
-    print "Enter the ID you wish to look for: "
-    id = gets.chomp
-    Contact.show(id)
-  when "find"
-    print "Enter a term you wish to search for: "
-    term = gets.chomp
-    puts Contact.find(term)
-  else 
-    puts "Wrong command!! Try another command" unless user_input == "help"
-    puts "Here is a list of available commands"
-    puts "new - Create a new contact"
-    puts "list - List all contacts"
-    puts "show - show a contact"
-    puts "find - Find a contact"
+if c1.valid? && c2.valid? && c3.valid? && c4.valid? && c5.valid?
+  puts "Contacts successfully added to Database"
+else
+  puts c1.errors.messaage
+end
+
+
+loop do 
+  puts "This is Ryan's contact list!"
+  puts "What would you like to do?"
+  puts "Enter 'Help' for a list of commands."
+  print '> '
+
+  user_input = gets.chomp
+
+
+  case user_input
+    # binding.pry
+    when "new"
+      print "First Name :"
+      first_name = gets.chomp
+      print "Last Name :"
+      last_name = gets.chomp
+      print "Email:"
+      email = gets.chomp
+      Contact.create(first_name: first_name,last_name: last_name, email: email)
+
+    when "list"
+      puts Contact.all
+    
+    when "show"
+      print "Enter the ID you wish to look for: "
+      id = gets.chomp
+      puts Contact.find(id)
+
+    when "search"
+      print "Enter a term you wish to search for: "
+      term = gets.chomp
+      puts Contact.search(term)
+    else 
+      puts "Wrong command!! Try another command" unless user_input == "help"
+      puts "Here is a list of available commands"
+      puts "new - Create a new contact"
+      puts "list - List all contacts"
+      puts "show - Show a contact"
+      puts "search - Search for a term"
+  end
+  break if user_input == "quit"
 end
